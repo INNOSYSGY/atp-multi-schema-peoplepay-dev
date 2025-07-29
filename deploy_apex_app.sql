742b945f5b862d6a16a809bd3c04034a98d17d97
leo-cuyar <77472850+leo-cuyar@users.noreply.github.com>
2025-07-29 13:43:52 -0400

    Create deploy_apex_app.sql

---
+++ deploy_apex_app.sql
@@ -0,0 +1,13 @@
+REM First setup the installation context in the target environment
+begin
+    apex_application_install.set_workspace('pplpayv8_auto');
+    apex_application_install.set_application_id(800);
+    apex_application_install.generate_offset();
+    apex_application_install.set_schema('TEMPLATE_APPV8');
+    apex_application_install.set_application_alias('MULTI-SCHEMA-PEOPLEPAY-DEPLOY-TESTS');  -- set same alias as create app before
+end;
+/
+REM Then install the application by running its exported SQL script
+REM For "split" export run @f112/install.sql 
+REM or @f112/install_component.sql instead
+@f112/install.sql
