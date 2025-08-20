e2e0ed353722a0cddd9718e4a168daca160d89bb
leo-cuyar <77472850+leo-cuyar@users.noreply.github.com>
2025-08-20 14:36:22 -0400

    Update deploy_apex_app.sql

--- deploy_apex_app.sql
+++ deploy_apex_app.sql
@@ -4,7 +4,7 @@ begin
     apex_application_install.set_application_id(800);
     apex_application_install.generate_offset();
     apex_application_install.set_schema('TEMPLATE_APPV8');
-    apex_application_install.set_application_name( p_application_name => 'Multi Schema PeoplePay - Main' );
+    apex_application_install.set_application_name( p_application_name => 'Multi Schema PeoplePay - $GITHUB_USERNAME' );
     apex_application_install.set_application_alias('MULTI-SCHEMA-PEOPLEPAY-DEPLOY-TESTS');  -- set same alias as create app before
 end;
 /
