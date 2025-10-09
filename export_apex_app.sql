4a1f65c9fce441632af218701f91df9232593e06
Leonardo Cuyar <leo.cuyar@gmail.com>
2025-10-09 19:31:39 +0000

    Backup - 20251009

---
+++ export_apex_app.sql
@@ -0,0 +1,12 @@
+set long 100000
+SET LINESIZE 200
+SET TRIMSPOOL ON
+SET HEAD OFF
+SET PAGESIZE 0
+SET FEEDBACK OFF
+SET VERIFY OFF
+SET ECHO OFF
+
+apex export -applicationid ''  -split  -overwrite-files -expSupportingObjects Y -expType APPLICATION_SOURCE ;
+
+apex export -applicationid '' -overwrite-files;
